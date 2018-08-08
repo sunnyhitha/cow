7ae6d3c178017289909606ff01fadb18d0056d0b
sunnyhitha <41725679+sunnyhitha@users.noreply.github.com>
2018-08-08 20:01:26 +0530

    Create cow.java

---
+++ cow.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   int n;
+	   Scanner sc=new Scanner(System.in);
+	   n=sc.nextInt();
+	   if(n%2==0)
+	   {
+	   	System.out.print("yes");
+	   }
+	   else if(n%2!=0)
+	   {
+	   	System.out.print("no");
+	   }
+	   // your code goes here
+	}
+}
